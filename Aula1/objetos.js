2eb920f8ad950870dedb6450ad5e43040db4679a
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-01-20 14:44:09 -0300

    acessando objetos

---
+++ Aula1/objetos.js
@@ -0,0 +1,8 @@
+const listaCPFs = [11111111, 2222222, 33333333];
+
+const informacoesPessoa = ["Nome", "José", "Idade", 32, "CPF", "2341234214"];
+
+const objetoPessoa = {
+    nome: "Pedro",
+    idade: 19,
+}
