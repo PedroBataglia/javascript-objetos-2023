f98fc1b3dc4bf70ec6ab2593e6f51991d01f8f66
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-01-23 16:02:01 -0300

    testando a manipulacao de objetos

---
+++ Aula2/manipulando-objetos.js
@@ -0,0 +1,20 @@
+const pessoa = {
+    nome: "Marcos",
+    idade: 34,
+    email: ["Marcos@firma.com", "marcosandeade@log.com"],
+    telefone: ["2384509", "09328475"]
+}
+
+pessoa.animalEstimacao = [{
+    nome: "Kripto",
+    raça: "Cão",
+    vacinado: true
+}];
+
+pessoa.animalEstimacao.push({
+    nome: "Ace",
+    raça: "cão",
+    vacinado: true
+});
+
+console.log(pessoa)
