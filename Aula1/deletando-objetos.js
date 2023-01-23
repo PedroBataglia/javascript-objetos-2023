77b6e7d427bd995d68dd6658bc184df7430066cc
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-01-23 15:57:08 -0300

    testando copia e referencia de objetos

--- Aula1/deletando-objetos.js
+++ Aula1/deletando-objetos.js
@@ -12,3 +12,13 @@ delete objPersonagem.aliado;
 
 console.log(objPersonagem);
 console.log(objPersonagem.aliado);
+
+const objPersonagem2 = objPersonagem;
+objPersonagem2.nome = "Gandalf, O cinzento"
+
+const objPersonagem3 =Object.create(objPersonagem)
+objPersonagem3.nome = "Gandalf"
+
+console.log(objPersonagem.nome);
+console.log(objPersonagem2.nome);
+console.log(objPersonagem3.nome);
---
+++ Aula1/testes-primitivos.js
@@ -0,0 +1,6 @@
+let num = 50
+let num2 = num
+
+num2 = 100
+console.log(num) //50
+console.log(num2) //100
