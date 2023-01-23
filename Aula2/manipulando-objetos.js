0c2f7372756a4fc34dfe11089b692c9a1ae2268b
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-01-23 16:09:15 -0300

    testando metodos

--- Aula2/manipulando-objetos.js
+++ Aula2/manipulando-objetos.js
@@ -8,15 +8,27 @@ const pessoa = {
 pessoa.animalEstimacao = [{
     nome: "Kripto",
     raça: "Cão",
-    vacinado: true
+    vacinado: true,
 }];
 
 pessoa.animalEstimacao.push({
     nome: "Ace",
     raça: "cão",
-    vacinado: true
+    vacinado: true,
 });
 
-delete pessoa.animalEstimacao[0]
+pessoa.animalEstimacao.push({
+    nome: "Brutos",
+    raça: "cão",
+    vacinado: false,
+});
+
+// delete pessoa.animalEstimacao[0]
 
 console.log(pessoa)
+
+ const animaisApenasVacinados = pessoa.animalEstimacao.filter(
+    (animal) => animal.vacinado === true
+ );
+
+ console.log(animaisApenasVacinados);
