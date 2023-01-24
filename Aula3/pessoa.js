759b5697f464500729e6da3fcda134ca56bf422e
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-01-24 11:40:15 -0300

    novas formas de declarar chaves e valores

---
+++ Aula3/pessoa.js
@@ -0,0 +1,13 @@
+let pessoa = new Object(),
+telefone = "Meu telefone",
+hobbie = "Minha paixao",
+nivel = "Minha formacao",
+idade = "minha idade"
+
+pessoa.animalEstimacao   = "gato";
+pessoa[telefone]         = "23 4481-3434";
+pessoa[hobbie]           = "Vioão";
+pessoa[nivel]            = "Iniciante";
+pessoa[idade]            = "19 anos";
+
+console.log(pessoa)
--- Aula3/trabalhando-objetos.js
+++ Aula3/trabalhando-objetos.js
@@ -8,3 +8,17 @@ meuCarro.ano = 1969;
 for (let campo in meuCarro) {
     console.log(`${campo} => ${meuCarro[campo]}`);
 };
+
+
+let meuObj = new Object(),
+str = "minha string",
+alert = Math.random(),
+obj = new Object();
+
+meuObj.tipo = "Sintaxe de ponto";
+meuObj["data de criacao"] = "string com espaco";
+meuObj[str] = "valor de string";
+meuObj[alert] = "número aleatório";
+meuObj[obj] = "Objeto";
+meuObj[""] = "Mesmo uma string vazia";
+console.log(meuObj);
