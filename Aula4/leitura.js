7e18ac0182dece92264ba68a8da799cbfb83514f
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-01-25 09:00:26 -0300

    transformando JSON em string e string em objetos

--- Aula4/leitura.js
+++ Aula4/leitura.js
@@ -5,3 +5,12 @@ console.log(lutador);
 console.log(dados);
 console.log(typeof dados);
 
+const clienteEmString = JSON.stringify(dados)
+
+console.log(clienteEmString);
+console.log(clienteEmString.email);
+console.log(typeof clienteEmString);
+
+const objCliente = JSON.parse(clienteEmString); 
+
+console.log(objCliente);
