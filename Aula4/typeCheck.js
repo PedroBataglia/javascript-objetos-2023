07556f30fd3855f7d80cba2f81a23cc187a0165a
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-01-25 19:10:38 -0300

    clonando objetos e arrays

--- Aula4/typeCheck.js
+++ Aula4/typeCheck.js
@@ -20,3 +20,23 @@ console.log(typeCheck(null));    // null
 console.log(typeCheck({}));      // object
 console.log(typeCheck('teste')); // string
 console.log(typeCheck(123));     // number
+
+
+// const cloneArray = (element) => {
+//     const clonedArray = []
+//     for (const item of element) {
+//         if (typeCheck(item) === 'array') clonedArray.push(cloneArray(item))
+//         else clonedArray.push(item)
+//     }
+//     return clonedArray
+// }
+
+const cloneArray = (element) => {
+    if (typeCheck(element) !== 'array') return element
+    return element.map(cloneArray)
+};
+
+const numbers = [1, 2, 3];
+const numbersCopy = numbers;
+console.log(numbers === numbersCopy); // true
+console.log(numbers === cloneArray(numbers)); // false
