bb91d1b86b98a805a891d60ed71bb1e9ba2c4652
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-01-27 08:37:07 -0300

    treinando utilizacao do for + logica

--- Treinando-logica.js
+++ Treinando-logica.js
@@ -1,6 +1,7 @@
 const nota1 = 5;
 const nota2 = 6;
 const nota3 = 10;
+const notas = [10, 5, 8, 3]
 let aprovado = "";
 
 const media = (nota1 + nota2 + nota3) / 3;
@@ -13,3 +14,11 @@ if (media >= 7) {
 
 
 console.log(`O aluno tem média ${media} e ele está ${aprovado}`);
+
+let somaNotas = 0;
+
+for (let i = 0; i < notas.length; i++) {
+    somaNotas += notas[i]/notas.length;
+}
+
+console.log(somaNotas)
