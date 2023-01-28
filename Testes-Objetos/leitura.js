8b9d0c2c41992abe6270db7646d484ab904416e5
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-01-28 18:23:36 -0300

    testando mais

--- Testes-Objetos/leitura.js
+++ Testes-Objetos/leitura.js
@@ -1,22 +1,22 @@
 const livros = require("./livros.json");
 
-// for (let i = 0; i < livros.length; i++) {
-//     console.log(livros[i]);
-// };
+ for (let i = 0; i < livros.length; i++) {
+     console.log(livros[i]);
+ };
 
 function ordenar(lista, propriedade) {
     const resultado = lista.sort((a, b) => {
-        if (a[propriedade] < b[propriedade]) {
-            return -1;
-        }
         if (a[propriedade] > b[propriedade]) {
             return 1;
+        }
+        if (a[propriedade] < b[propriedade]) {
+            return -1;
         }
         return 0;
     });
     return resultado;
 };
 
-const listaOrdenada = ordenar(livros, "nota");
+const listaOrdenada = ordenar(livros, "nome");
 
-console.log(ordenar(livros, "ano"));
+console.log(listaOrdenada);
