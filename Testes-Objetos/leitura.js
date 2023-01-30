246258773ddd6d0fc4ec339b750bfbaa56f5edf8
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-01-30 09:11:50 -0300

    tentando fazer a ordem funcionar

--- Testes-Objetos/leitura.js
+++ Testes-Objetos/leitura.js
@@ -1,22 +1,25 @@
 const livros = require("./livros.json");
 
- for (let i = 0; i < livros.length; i++) {
-     console.log(livros[i]);
- };
+//  for (let i = 0; i < livros.length; i++) {
+//      console.log(livros[i]);
+//  };
 
-function ordenar(lista, propriedade) {
+function ordenar(lista, propriedade, ordem = "crescente") {
     const resultado = lista.sort((a, b) => {
         if (a[propriedade] > b[propriedade]) {
             return 1;
-        }
+        };
         if (a[propriedade] < b[propriedade]) {
             return -1;
-        }
+        };
         return 0;
     });
+    if(ordem !== "crescente") {
+        resultado.reverse();
+    }
     return resultado;
 };
 
-const listaOrdenada = ordenar(livros, "nome");
+const listaOrdenada = ordenar(livros, "ano", "reversa");
 
 console.log(listaOrdenada);
