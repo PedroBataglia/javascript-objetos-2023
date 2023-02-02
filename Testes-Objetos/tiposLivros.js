d5208f48faf4f522e5246ab34ed1fe6c92cae6a1
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-02-02 15:36:33 -0300

    filtrando por titulos duplos e exibindo apenas o segundo

---
+++ Testes-Objetos/tiposLivros.js
@@ -0,0 +1,31 @@
+const livros = require("./livros.json");
+let armazenar = "";
+
+
+//  for (let i = 0; i < livros.length; i++) {
+//     if (typeof(livros[i].Nome) === "object") {
+//         console.log(livros[i]["Nome"])
+//     } else {
+//         continue;
+//     }
+//  }
+
+function livrosObjetos (lista) {
+    const livrosComDoisTitulos = lista.filter((livro) => {
+        if (typeof(livro.Nome) === "object") {
+            return livro;
+        } else {
+            
+        }
+    });
+    return livrosComDoisTitulos;
+}
+
+const listaDeTitulos = livrosObjetos(livros);
+
+
+//console.log(listaDeTitulos);
+
+for (let i = 0; i < listaDeTitulos.length; i++) {
+    console.log(listaDeTitulos[i]["Nome"][1])
+}
