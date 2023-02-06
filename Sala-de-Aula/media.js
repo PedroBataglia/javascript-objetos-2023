97e2e0329259f45358130e0b19a64423393633dd
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-02-06 09:04:06 -0300

    analisando a media para definir se foi aprovado

--- Sala-de-Aula/media.js
+++ Sala-de-Aula/media.js
@@ -6,7 +6,22 @@ function media(lista, indice) {
         soma += (lista[indice]["notas"][i])
     }
     const total = soma / lista[indice]["notas"].length; 
+    lista[indice].media = total;
     return total
 }
 
 console.log(media(alunos, 1));
+
+//console.log(alunos);
+
+function aprovado(mediaAluno, media) {
+    if (mediaAluno["media"] >= media) {
+        mediaAluno.situacao = "aprovado"
+    } else {
+        mediaAluno.situacao = "reprovado"
+    }
+}
+
+aprovado(alunos[1], 7);
+
+console.log(alunos)
