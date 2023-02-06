892b28e85e1dabbe1bd115c8bb3fe69dc49df4ac
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-02-06 08:53:06 -0300

    atualizando o coidgo

--- Sala-de-Aula/sala.js
+++ Sala-de-Aula/sala.js
@@ -1,13 +1,4 @@
 const alunos = require("./alunos.json");
 
 
-
-function encontrar(lista, chave, valor) {
-    const encontrado = lista.find((item) => item[chave].includes(valor))
-
-    return encontrado
-}
-
-const alunoEncontrado = encontrar(alunos, "Nome", "Maria");
-
-console.log(alunoEncontrado)
+console.log(alunos[1]["notas"]);
